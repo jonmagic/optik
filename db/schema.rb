b2f260beca56b56abf3f03a7bb7a875778b3d7e6
dcparker <dcparker@331dd862-6008-0410-a02f-b57fe9264243>
2007-11-08 20:34:02 +0000

    up-to-date schema
    git-svn-id: http://store.sabretechllc.com/public/jonmagic/optik/trunk@296 331dd862-6008-0410-a02f-b57fe9264243

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,63 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 8) do
 
   create_table "engine_schema_info", :id => false, :force => true do |t|
-    t.column "engine_name", :string
-    t.column "version",     :integer
+    t.column "engine_name", :string,                :default => "NULL"
+    t.column "version",     :integer, :limit => 11, :default => 0
+  end
+
+  create_table "notes", :force => true do |t|
+    t.column "content",    :text
+    t.column "ticket_id",  :integer,  :limit => 11, :default => 0
+    t.column "user_id",    :integer,  :limit => 11, :default => 0
+    t.column "created_at", :datetime
+    t.column "updated_at", :datetime
+  end
+
+  add_index "notes", ["id"], :name => "sqlite_autoindex_notes_1", :unique => true
+
+  create_table "states", :force => true do |t|
+    t.column "description", :string,                :default => "NULL"
+    t.column "priority",    :integer, :limit => 11, :default => 0
+  end
+
+  create_table "tags", :force => true do |t|
+    t.column "name", :string, :default => "NULL"
+  end
+
+  create_table "tags_tickets", :id => false, :force => true do |t|
+    t.column "ticket_id", :integer, :limit => 11, :default => 0
+    t.column "tag_id",    :integer, :limit => 11, :default => 0
+  end
+
+  create_table "tickets", :force => true do |t|
+    t.column "description", :string,                 :default => "NULL"
+    t.column "user_id",     :integer,  :limit => 11, :default => 0
+    t.column "state_id",    :integer,  :limit => 11, :default => 0
+    t.column "parts",       :string,                 :default => "NULL"
+    t.column "referrals",   :string,                 :default => "NULL"
+    t.column "created_at",  :datetime
+    t.column "updated_at",  :datetime
+  end
+
+  create_table "users", :force => true do |t|
+    t.column "login",           :string,   :limit => 80, :default => "''",   :null => false
+    t.column "salted_password", :string,   :limit => 40, :default => "''",   :null => false
+    t.column "email",           :string,   :limit => 60, :default => "''",   :null => false
+    t.column "firstname",       :string,   :limit => 40, :default => "NULL"
+    t.column "lastname",        :string,   :limit => 40, :default => "NULL"
+    t.column "salt",            :string,   :limit => 40, :default => "''",   :null => false
+    t.column "verified",        :integer,  :limit => 11, :default => 0
+    t.column "role",            :string,   :limit => 40, :default => "NULL"
+    t.column "security_token",  :string,   :limit => 40, :default => "NULL"
+    t.column "token_expiry",    :datetime
+    t.column "created_at",      :datetime
+    t.column "updated_at",      :datetime
+    t.column "logged_in_at",    :datetime
+    t.column "deleted",         :integer,  :limit => 11, :default => 0
+    t.column "delete_after",    :datetime
   end
 
 end
