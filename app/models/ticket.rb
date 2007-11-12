92d500ed7756fbbc1dc69dbc495b454ec59a73a9
dcparker <dcparker@331dd862-6008-0410-a02f-b57fe9264243>
2007-11-12 16:38:59 +0000

    fixed the sorting for new ticket similar search?
    git-svn-id: http://store.sabretechllc.com/public/jonmagic/optik/trunk@305 331dd862-6008-0410-a02f-b57fe9264243

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -39,28 +39,17 @@ class Ticket < ActiveRecord::Base
     if !query.to_s.strip.empty?
       tokens = query.split
       resultshash = {}
+      offset = options.delete(:offset) || 0
+      limit = options.delete(:limit) || 100000
       Ticket.find_tagged_with(options.merge(:any => tokens)).each do |result|
         resultshash[result.id] ||= [0, nil]
         resultshash[result.id][0] += 1
         resultshash[result.id][1] = result
       end
       results = resultshash.keys.collect {|k| resultshash[k][1]}.sort {|a,b| b.updated_at <=> a.updated_at}
-        offset = options.delete(:offset) || 0
-        limit = options.delete(:limit) || 100000
       return results[offset..(offset+limit)]
     else
       []
     end
   end
-
-#   def self.search(query)
-#     if !query.to_s.strip.empty?
-#       tokens = query.split.collect {|c| "%#{c.downcase}%"}
-#       find_by_sql(["SELECT * from tickets WHERE #{ (["(LOWER(description) LIKE ?)"] * tokens.size).join(" AND ") }", *tokens.collect { |token| [token] * 1 }.flatten])
-#     else
-#       []
-#     end
-#   end
-
-  
 end
